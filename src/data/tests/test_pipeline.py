bf583ff0aa49763d294cfcb729cc03759f692172
shreya <shreya.pandit25@gmail.com>
2018-05-30 13:30:52 -0400

    change test config path, remove some debug code

--- src/data/tests/test_pipeline.py
+++ src/data/tests/test_pipeline.py
@@ -21,23 +21,15 @@ def test_entire_pipeline_should_run_without_errors(tmpdir):
     city_level_data_path_raw = boston_path +'raw/'
     shutil.copytree(orig_path+'/raw/', city_level_data_path_raw)
 
-    # if not os.path.exists(city_level_data_path):
-    #     print "no dir for city level data, creating one in " + path
-    #     shutil.copytree(orig_path+'standardized/', city_level_data_path)
-        
-    # os.makedirs(boston_path + 'raw/crashes/')
     os.makedirs(boston_path + 'standardized')
 
     if not os.path.exists(boston_path + 'raw/crashes/cad_crash_events_with_transport_2016_wgs84_with_modetype.csv'):
         print 'No raw crashes here...'
-        # f = open(boston_path + 'raw/crashes/crashes.csv',"w+")
-        # f.close()
+
 
     if os.path.exists(boston_path + 'raw/crashes/cad_crash_events_with_transport_2016_wgs84_with_modetype.csv'):
         print 'crashes here...'
-        # f= open(boston_path + 'raw/crashes/crashes.csv',"w+")
 
-    # shutil.copy2(orig_path+'raw/'+'crashes.csv', boston_path+'raw/crashes/crashes.csv')   
     subprocess.check_call([
         'python',
         '-m',

81a3f352197a0960ae719ef36019d031291fac77
shreya <shreya.pandit25@gmail.com>
2018-09-09 13:59:26 -0400

    sample test for make js config

--- src/data/tests/test_initialize_city.py
+++ src/data/tests/test_initialize_city.py
@@ -5,24 +5,24 @@ import shutil
 import yaml
 import py
 
-def test_initialize_city_should_run_successfully(tmpdir):
 
+def test_initialize_city_should_run_successfully(tmpdir):
     # Copy test data into temp directory
     orig_path = os.path.dirname(
         os.path.abspath(__file__)) + '/data/'
     path = os.path.join(tmpdir.strpath, 'data')
-    raw_files_path = os.path.join(path ,'test_initialize_city')
+    raw_files_path = os.path.join(path, 'test_initialize_city')
     shutil.copytree(orig_path, path)
 
-    #other constants that the test needs to pass in
+    # other constants that the test needs to pass in
     new_city_folder = 'new_city'
 
-    #create a config folder in temp directory prior to running the test
-    if not os.path.exists(os.path.join(tmpdir.strpath,'config')):
+    # create a config folder in temp directory prior to running the test
+    if not os.path.exists(os.path.join(tmpdir.strpath, 'config')):
         print("no config dir, creating one...")
-        os.makedirs(os.path.join(tmpdir.strpath,'config'))
+        os.makedirs(os.path.join(tmpdir.strpath, 'config'))
         os.makedirs(os.path.join(tmpdir.strpath, 'reports'))
-    
+
     subprocess.check_call([
         'python',
         '-m',
@@ -34,16 +34,18 @@ def test_initialize_city_should_run_successfully(tmpdir):
         '-configPath',
         os.path.join(tmpdir.strpath, 'config/'),
         '-crash',
-        os.path.join(raw_files_path,'Crashes_new_city.csv'),
+        os.path.join(raw_files_path, 'Crashes_new_city.csv'),
         '-concern',
-        os.path.join(raw_files_path,'Concerns_new_city.csv'),
+        os.path.join(raw_files_path, 'Concerns_new_city.csv'),
         '-f',
         new_city_folder
     ])
 
 
-def test_initialize_city_brisbane(monkeypatch):
+def test_make_js_config(monkeypatch):
+    assert True
 
+def test_initialize_city_brisbane(monkeypatch):
     def mockreturn(address):
         return "Brisbane, Australia", -27.4697707, 153.0251235, 'S'
 

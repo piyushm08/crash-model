2162d804e1ca8583bd0d14522f771f3106ecfaea
shreya <shreya.pandit25@gmail.com>
2019-03-27 11:19:44 -0400

    updated gitignore, keep only files needed for viz

--- src/data/TMC_scraping/__init__.py
+++
@@ -1,3 +0,0 @@
-"""
-This package parses turning movement count files
-"""
--- src/data/segment.py
+++
@@ -1,9 +0,0 @@
-
-class Segment(object):
-    "A segment contains a dict of properties and a shapely shape"
-
-    def __init__(self, geometry, properties):
-        
-        self.geometry = geometry
-        self.properties = properties
-
